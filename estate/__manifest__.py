f864274fc59af61e38ef737d7886cfc58ed3cd1f
Pavel Vodrážka <Pavel.Vodrazka@gmail.com>
2024-08-19 12:41:58 +0200

    Initial UI added.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,9 @@
         'base'
     ],
     'data': [
-         'security/ir.model.access.csv'   
+         'security/ir.model.access.csv',
+         'views/estate_property_views.xml',
+         'views/estate_property_menus.xml'
     ],
     'application': True
 }
